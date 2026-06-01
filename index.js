b4fc3487d9db5a8b3c2677e7e5e7d7346b4ac440
raviwed <ravitejabtech964@gmail.com>
2026-06-01 23:20:48 +0530

    implement the today extenstions and particed problems

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-const {add, sub} = require("./src/math")
+const { add, sub } = require("./src/math")
+const http = require("http")
+const fs = require("fs")
+console.log("Hello World", add(2, 5), sub(5, 4))
 
-console.log("Hello World", add(2,5), sub(5,4))
+const myServer = http.createServer((req, res) => {
+    console.log("New Request Rec.", req.headers);
+    const log=`${Date.now()}:${req.url}New Request Received`
+    fs.appendFile("log.txt",log,(err,data)=>{
+        switch(req.url){
+          case "/":
+           res.end("Home Page");
+          break
+          case "/about":
+            res.end("this is About Page")
+        case "/takeOut":
+            res.end("this End is About page")
+            default:
+              res.end("404")    
+        }
+    })
+    // res.end("Hello from Server");
+})
+
+myServer.listen(8000, () => console.log("Server Started!"));
