e7fc3263730b2b620284a17e7e06f9de6362f022
raviwed <ravitejabtech964@gmail.com>
2026-06-09 20:53:01 +0530

    Authorization

--- middlewares/authUser.js
+++ middlewares/authUser.js
@@ -1,8 +1,21 @@
 const { getUser } = require("../services/authUser");
 const cookieparser = require("cookie-parser")
 // const {}=require("/")
+
+
+
+function checkForAuthentication(req,res,next){
+    const authorizationHeaderValue=req.headers["authorization"]
+    const token = userUid.split("Bearer")[1];
+    if(!authorizationHeaderValue || !authorizationHeaderValue.startWith("")){
+        return next()
+    }
+    const token=authorizationHeaderValue.split("Bearer")[1]
+    getUser()
+}
 async function resistrictToLoggedinUserOnly(req, res, next) {
     const authUserId = req.cookies.uid;
+    
     if (!authUserId) return res.redirect('/login')
     const user = getUser(userUid)
     req.user = user;
