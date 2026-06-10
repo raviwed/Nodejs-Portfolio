75eb90e53d1873be1267af9732796f7d137dcd5e
illuri raviteja reddy <107460626+raviwed@users.noreply.github.com>
2026-06-10 09:18:44 +0530

    Delete routes/authuser.js

--- routes/authuser.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const { handleAuthUserSignUp,handleAuthLoginUser } = require('../controllers/authuser')
-const router = express.Router();
-
-router.post('/', handleAuthUserSignUp )
-router.post('/login', handleAuthLoginUser)
-module.exports=router
