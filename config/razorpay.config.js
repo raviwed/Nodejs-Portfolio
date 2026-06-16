f400596eee6e0c70f410e941c4c5175f9a2fceb3
raviwed <ravitejabtech964@gmail.com>
2026-06-16 19:44:51 +0530

    payment gate Way Project

--- config/razorpay.config.js
+++ config/razorpay.config.js
@@ -1,12 +1,12 @@
-const razorpay =require("razorpay");
+const razorpay = require("razorpay");
 
-const dotenv =require("dotenv");
+const dotenv = require("dotenv");
 dotenv.config();
 
-const createRazorpayInstance=()=>{
-   return new razorpay({
-    key_id:process.env.RAZORPAY_KEY_ID,
-    key_secret:process.env.RAZORPAY_KEY_SECRET,
-})
+const createRazorpayInstance = () => {
+    return new razorpay({
+        key_id: process.env.RAZORPAY_KEY_ID,
+        key_secret: process.env.RAZORPAY_KEY_SECRET,
+    })
 }
-
+module.exports = { createRazorpayInstance }
--- controller/payments.controller.js
+++ controller/payments.controller.js
@@ -1,8 +1,38 @@
-exports.createOrader=async(req,res)=>{
-   const {courseId,amount}=req.body;
-const options={
-    amount:amount*1000,
-    currency:"INR",
-    recepit:"receipt_order_1"
+const { createRazorpayInstance } = require('../config/razorpay.config')
+const dotenv = require("dotenv");
+dotenv.config();
+
+const razorpayInstance = createRazorpayInstance();
+
+exports.createOrder = async (req, res) => {
+    const { courseId, amount } = req.body;
+    const options = {
+        amount: amount * 1000,
+        currency: "INR",
+        recepit: "receipt_order_1"
+    }
+
+    try {
+        razorpayInstance.orders.create(options, (err, order) => {
+            if (err) {
+                return res.status(500).json({
+                    success: false,
+                    message: "Some thing went Wrong"
+                })
+            }
+            return res.status(200).json(order)
+        })
+
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: "Something went wrong"
+        })
+        console.log(err)
+    }
+
 }
+exports.veriFyPayment = async (req, res) => {
+    const { order_id, payment_id, signature } = req.body;
+    const secret = process.env.RAZORPAY_KEY_SECRET;
 }
