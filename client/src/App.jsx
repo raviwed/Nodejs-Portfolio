8dcd50afba5ed026ab3bf8f24d968e8f7b848247
raviwed <ravitejabtech964@gmail.com>
2026-06-27 00:05:54 +0530

    chat rooms

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,121 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { io } from "socket.io-client"
 import './App.css'
+import { useEffect } from 'react';
+import { Container, TextField, Typography, Button } from "@mui/material";
+import { useState , useMemo} from "react";
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("");
+  const socket = useMemo(() => io('http://localhost:8000'), []);
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("connected", socket.id);
+    });
+
+    socket.on("receive-message",(data)=>{
+     console.log("received message:", data)
+    })
+
+    return () => {
+      socket.off("connect");
+      socket.off("receive-message");
+      socket.disconnect();
+    };
+  }, [socket])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("sent message:", message);
+    socket.emit("message", message);
+    setMessage("");
+  }
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+    <Container maxWidth="sm" >
+      <Typography variant="h1" component="div" gutterBottom >
+        Welcome to Socket.io
+      </Typography>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          id="outlined-basic"
+          label="Message"
+          variant="outlined"
+        />
+        <Button type="submit" variant="contained" color="primary" >
+          send
+        </Button>
+      </form>
+    </Container>
   )
 }
 
--- server/index.js
+++ server/index.js
@@ -1,8 +1,49 @@
 const http=require("http")
 const express=require("express");
 const app=express();
-const {Server}=require("socket.io")
+const {Server}=require("socket.io");
+const cors=require("cors");
 const server=http.createServer(app);
-// const io=new Server(app);
+const clientOrigin="http://localhost:5173";
 
-server.listen(8000,()=>console.log("Server Started "))
+const io=new Server(server,{
+    cors:{
+        origin:clientOrigin,
+        methods:["GET","POST"],
+        credentials:true,
+    }
+});
+
+app.use(cors({
+        origin:clientOrigin,
+        methods:["GET","POST"],
+        credentials:true,
+    }));
+
+io.on("connection",(socket)=>{
+  console.log("User connection")
+  console.log("Id",socket.id)
+  socket.emit("welcome",`welcome to the server`)
+  socket.broadcast.emit("welcome",`Welcome to sever,${socket.id}`)
+  socket.on("message",({room,message})=>{
+    console.log("message Value",{room,message});
+    socket.broadcast.emit("receive-message",message)
+    // io.emit("message",data);
+  })
+  socket.on("disconnect",(data)=>{
+    console.log(data)
+    io.emit("receive-message",data);
+  })
+  socket.on("receive-message",(data)=>{
+    console.log("receive-message",data)
+  })
+  return ()=>{
+   socket.disconnect();
+  }
+})
+
+// app.get("/",(req,res)=>{
+//     res.send("value added");
+// })
+
+server.listen(8000,()=>console.log("Server Started"));
